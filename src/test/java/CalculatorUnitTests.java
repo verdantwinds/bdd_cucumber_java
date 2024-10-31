2fb12b5fda8ffde4f3abef98d1cfeb0bef0039b8
verdantwinds <verdantwind6@gmail.com>
2024-10-31 11:52:29 +0000

    updated name

--- src/test/java/CalculatorUnitTests.java
+++ src/test/java/CalculatorUnitTests.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AdditionUnitTests {
+public class CalculatorUnitTests {
 
     @Test
     public void add_int_1_and_int_2_returns_3() {
